fe213558e21d3224141a89905c2af23a8138df4c
wettingchou <wtchou1209@gmail.com>
2016-07-05 00:15:32 +0800

    init commit

---
+++ spec/objectStore.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+var assert = require('assert');
+var Store = require('../lib');
+var fs = require('fs');
+
+describe('Object Store', function() {
+  var store;
+  before(function(done) {
+    store = new Store({ bucket: 'MOCKUP' });
+    done();
+  });
+
+  it('should create a new object in the store', function(done) {
+    var key = ['posts', 'aad2d60bb2d54400', 'SHARDING', 'pano', 'src', '2016-07-01', 'test.jpg'];
+    var file = fs.readFileSync('./fixture/1.jpg');
+    store.create(key, file, function(err, result) {
+      assert(err);
+      var regex = /(posts)\/(aad2d60bb2d54400)\/([a-z0-9]{4})\/(pano)\/(src)\/([0-9-]+\/([a-z.]+))/;
+      assert(result['Key'].match(regex), true);
+      done();
+    });
+  });
+});
